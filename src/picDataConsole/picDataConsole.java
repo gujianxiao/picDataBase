5595bfd7b938a69227641f1ccd4008ec099a140a
gujianxiao <cb361991@icloud.com>
2015-07-15 17:01:55 +0800

    add the func of pm2.5
    add the func of pm2.5,but can only read the specifical data

--- src/picDataConsole/picDataConsole.java
+++ src/picDataConsole/picDataConsole.java
@@ -3,6 +3,7 @@ package picDataConsole;
 import picDataConsole.databean;
 import java.awt.Container;  
 import java.awt.FlowLayout;
+import java.awt.List;
 import java.awt.TextField;
 
 import java.awt.GridLayout;  
@@ -16,11 +17,17 @@ import javax.swing.JPanel;
 import javax.swing.JScrollPane;  
 import javax.swing.JTextArea;  
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Date;
 import java.util.Iterator;
@@ -75,7 +82,105 @@ public class picDataConsole {
     		JFrame frame=new JFrame("PicDataConsole");
     		final JTextArea picDir=new JTextArea();
     		JButton button=new JButton("选择文件夹");
+    		JButton pm=new JButton("导入 pm2.5 数据");
      	    final Container content = frame.getContentPane(); 
+     	    MouseListener pm25=new MouseListener(){
+
+				@Override
+				public void mouseClicked(MouseEvent e) {
+					// TODO Auto-generated method stub
+                    databean insert=new databean();
+                    Connection conn = null;
+                    ResultSet rs=null;
+                    insert.setDB("pic data");//set database name
+                    conn=insert.getConn();//connect to database
+                    String sql=null;
+					 if (e.getClickCount() == 2) { 
+					 fileChooser.setFileSelectionMode(fileChooser.FILES_ONLY);
+					 fileChooser.setDialogTitle("打开pm2.5数据文件");
+	            	 int ret = fileChooser.showOpenDialog(null);
+	            	 if (ret == JFileChooser.APPROVE_OPTION) {
+	            		//文件选取
+	            		File pmdata=fileChooser.getSelectedFile();
+                      
+							BufferedReader pmReader;
+							try {
+								pmReader = new BufferedReader(new FileReader(pmdata));
+								String temp=null;
+								String[] tp=null;
+								ArrayList<String[]> pmValue=new ArrayList<String[]>();
+								while((temp=pmReader.readLine())!= null){
+									tp=temp.split(",");
+									pmValue.add(tp);
+								}
+								// select the range of time to detect error
+                            	String searchSql="select Min(time) from picdata where location='北邮'";                         	
+                            	rs=insert.executeSQL(searchSql);
+                            	rs.next();
+                            	String minTime=rs.getString(1);
+                            	searchSql="select Max(time) from picdata where location='北邮'";
+                            	rs=insert.executeSQL(searchSql);
+                            	rs.next();
+                            	String maxTime=rs.getString(1);
+                                //   iterator the pmValue to update database
+                                for(int i=0;i<pmValue.size();i++){
+                                	String[] dl1=pmValue.get(i);
+                               	String[] dl2=pmValue.get(i+1);                              	
+                                	if(dl1[0].equalsIgnoreCase("001008")){
+                                		String beginTime=dl1[1].replace('-', ':');
+                                		String endTime=dl2[1].replace('-', ':');
+                                		if((beginTime.compareTo(minTime)>0)&&(endTime.compareTo(maxTime)<0)){
+                                        String updateSql="update picdata set pm25='"+dl1[2]+"',pm10='"+dl1[3]+"',co2='"+dl1[4]+"' where '"+beginTime+"'<time and time<'"+endTime+"'";
+        								insert.executeUpdateSQL(updateSql); 		
+                                		}
+                                		else{
+                                			System.out.println("out of the time range!");
+                                		}
+                                	}
+                                }
+								
+							} catch (IOException e1) {
+								// TODO Auto-generated catch block
+								e1.printStackTrace();
+							} catch (SQLException e1) {
+								// TODO Auto-generated catch block
+								e1.printStackTrace();
+							}
+			
+	            		}
+						
+						 
+						 
+					 }
+					 
+					
+				}
+
+				@Override
+				public void mousePressed(MouseEvent e) {
+					// TODO Auto-generated method stub
+					
+				}
+
+				@Override
+				public void mouseReleased(MouseEvent e) {
+					// TODO Auto-generated method stub
+					
+				}
+
+				@Override
+				public void mouseEntered(MouseEvent e) {
+					// TODO Auto-generated method stub
+					
+				}
+
+				@Override
+				public void mouseExited(MouseEvent e) {
+					// TODO Auto-generated method stub
+					
+				}
+     	    	
+     	    };
     		 MouseListener ml = new MouseListener() {  
     	         public void mouseClicked(MouseEvent e) {  
     	             if (e.getClickCount() == 2) {  
@@ -138,7 +243,7 @@ public class picDataConsole {
 								ResultSet latest=insert.executeSQL(lastdata);
 									if(!latest.next()){// while there is no same picture,then insert it into database
 									addcount++;
-								String sql="insert into picdata values(null,'"+path+"','"+name+"','"+model+"','"+location+"','"+dpi+"','"+time+"')";
+								String sql="insert into picdata values(null,'"+path+"','"+name+"','"+model+"','"+location+"','"+dpi+"','"+time+"',null,null,null)";
 						        int r=insert.executeUpdateSQL(sql);
 						        //print the pic info for debug
 								System.out.println(time);
@@ -191,6 +296,7 @@ public class picDataConsole {
     	         }  
     	            }; 
     	   button.addMouseListener(ml);
+    	   pm.addMouseListener(pm25);
     	   JPanel txt=new JPanel();
     	   picDir.setColumns(15);
     	   picDir.setRows(1);
@@ -198,7 +304,8 @@ public class picDataConsole {
     	   content.setLayout(new FlowLayout(FlowLayout.LEFT));  
     	   content.add(txt);
     	   content.add(button);
-    	   frame.setSize(400,500);
+    	   content.add(pm);
+    	   frame.setSize(500,600);
     	   frame.setVisible(true);
     	   frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); 
        }
